60a14d9e7de3f2449bbea21402b1b1c70729d41a
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-02-22 17:46:31 +0200

    token

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { SuccessResponse } from "../../common/utils/responce/index.js";
-import {signup,login} from './auth.service.js'
+import {signup,login, getUserById} from './auth.service.js'
 
 const router = Router()
 
@@ -16,6 +16,12 @@ router.post('/login', async (req, res) => {
     let loginUser = await login(req.body)
     return SuccessResponse({res,message :'user login successfully ',status:200,data:loginUser})
 
+})
+
+
+router.get('/get-user-by-id',async (req,res)=>{
+    let userData = await getUserById(req.headers)
+    res.json(userData)
 })
 
 export default router
--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,9 +1,9 @@
 import { compareHash, generateHash } from '../../common/hash/hash.js'
 import { ProviderEnums } from '../../common/index.js'
-import { ConflictException, NotFoundException } from '../../common/utils/responce/index.js'
-import { findOne, insertOne } from '../../database/database.service.js'
+import { ConflictException, NotFoundException, UnauthorizedException } from '../../common/utils/responce/index.js'
+import { find, findById, findOne, insertOne } from '../../database/database.service.js'
 import { userModel } from '../../database/index.js'
-
+import jwt from 'jsonwebtoken'
 
 
 
@@ -28,13 +28,19 @@ export const login = async (data) => {
     let { email, password } = data
 
 
-    let existUser = await findOne({ model: userModel, filter: { email, provider: ProviderEnums.System }, select: '-_id' })
+    let existUser = await findOne({ model: userModel, filter: { email, provider: ProviderEnums.System } })
 
     if (existUser) {
 
         let isMatched = compareHash(password, existUser.password)
         if (isMatched) {
-            return existUser
+
+            let token = jwt.sign({ id: existUser._id }, "route", { expiresIn: '1d' })
+            console.log(existUser._id);
+
+            console.log(token);
+
+            return { existUser, token }
         }
     }
 
@@ -43,4 +49,23 @@ export const login = async (data) => {
 }
 
 
+
+export const getUserById = async (headers) => {
+
+    let { authorization } = headers
+
+    if (!authorization) {
+        UnauthorizedException({ message: "un authorized" })
+    }
+    
+    let decoded = jwt.verify(authorization, "route")
+
+    let userData = await findById({ model: userModel, id: decoded.id })
+    if (userData) {
+        return userData
+    }
+
+    return NotFoundException({ message: "user not found" })
+
+}
 
