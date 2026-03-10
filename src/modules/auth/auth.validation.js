2cc0ff1f68a4d7d4571d3f8a68433a6be596c5e2
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-03-11 01:36:10 +0200

    block of updates

--- src/modules/auth/auth.validation.js
+++ src/modules/auth/auth.validation.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const signupSchema = Joi.object().keys({
     userName: Joi.string().min(3).max(50).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(3).max(30).alphanum().required(),
+    password: Joi.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/).required(),
     gender: Joi.string().valid("male", "female"),
     dateOfBirth: Joi.date(),
     phone: Joi.string().pattern(new RegExp(/^(00201|\+20|01)[0|1|2|5]\d{8}$/)).required(),
@@ -13,7 +13,11 @@ export const signupSchema = Joi.object().keys({
 
 
 export const signinSchema = Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().min(3).max(30).alphanum().required(),
+    email: Joi.string().email().required().messages({
+        "string.email": "invalid email format",
+        "any.required": "email is required",
+        "string.empty": "email cannot be empty"
+    }),
+    password: Joi.string().pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/).required(),
 
 })
