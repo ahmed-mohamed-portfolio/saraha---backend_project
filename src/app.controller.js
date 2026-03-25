8e7191e1fa873077aefefe22c906abe49625e332
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-03-25 07:00:13 +0200

    OPEN CORES TO NETLIFY

--- src/app.controller.js
+++ src/app.controller.js
@@ -13,7 +13,14 @@ export const bootstrap = async () => {
 
     const app = express()
     app.use(express.json())
-    app.use(cors({ origin: "*" }))
+    app.use(cors({
+        origin: [
+            "http://localhost:4200",
+            "https://lucky-faloodeh-711088.netlify.app"
+        ],
+        credentials: true
+    }));
+
     app.use(express.urlencoded({ extended: true }));
     app.use('/upload', express.static("upload"))
 
