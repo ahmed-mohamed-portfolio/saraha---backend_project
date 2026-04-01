316a61b68e8a113020c65bf9894f56a24dffee8d
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-04-01 21:22:48 +0200

    try req.cookies.accessToken

--- src/common/middleWare/auth.js
+++ src/common/middleWare/auth.js
@@ -6,51 +6,77 @@ import { get } from '../services/redis.service.js'
 
 export const authentication = async (req, res, next) => {
 
-    let { authorization } = req.headers
-    console.log("authorization", authorization);
+    // let { authorization } = req.headers
 
-    if (!authorization) {
+    // if (!authorization) {
+    //     UnauthorizedException({ message: "un authorized" })
+    // }
+
+
+
+    // const [flag, token] = authorization.split(' ')
+
+    // switch (flag) {
+
+    //     case "Basic":
+    //         let data = Buffer.from(token, 'base64').toString()
+    //         let [email, password] = data.split(':')
+    //         console.log(email, " ", password);
+
+    //         break;
+
+    //     case 'Bearer':
+    //         let decodedData = decodeToken(token)
+
+
+    //         let user = await findById({ model: userModel, id: decodedData.id })
+
+    //         if (Math.floor(new Date(user.credentialsUpdatedAt).getTime() / 1000) > decodedData.iat) {
+    //             return BadRequestException({ message: 'invalid token22' })
+    //         }
+
+
+    //         req.userId = decodedData.id
+    //         req.jti = decodedData.jti
+
+    //         let blocked_Token = await get(`RevokeToken::${req.userId}::${req.jti}`)
+    //         if (blocked_Token !== null) {
+    //             return BadRequestException({ message: 'invalid token (blocked)' })
+    //         }
+
+    //         break;
+
+    //     default:
+    //         throw BadRequestException({ message: "missing authentication schema" })
+    //         break;
+    // }
+
+
+
+
+    if (!req.cookies.accessToken) {
         UnauthorizedException({ message: "un authorized" })
     }
 
+    let decodedData = decodeToken(req.cookies.accessToken)
 
 
-    const [flag, token] = authorization.split(' ')
+    let user = await findById({ model: userModel, id: decodedData.id })
 
-    switch (flag) {
-
-        case "Basic":
-            let data = Buffer.from(token, 'base64').toString()
-            let [email, password] = data.split(':')
-            console.log(email, " ", password);
-
-            break;
-
-        case 'Bearer':
-            let decodedData = decodeToken(token)
-
-
-            let user = await findById({ model: userModel, id: decodedData.id })
-
-            if (Math.floor(new Date(user.credentialsUpdatedAt).getTime() / 1000) > decodedData.iat) {
-                return BadRequestException({ message: 'invalid token22' })
-            }
-
-
-            req.userId = decodedData.id
-            req.jti = decodedData.jti
-
-            let blocked_Token = await get(`RevokeToken::${req.userId}::${req.jti}`)
-            if (blocked_Token !== null) {
-                return BadRequestException({ message: 'invalid token (blocked)' })
-            }
-
-            break;
-
-        default:
-            throw BadRequestException({ message: "missing authentication schema" })
-            break;
+    if (Math.floor(new Date(user.credentialsUpdatedAt).getTime() / 1000) > decodedData.iat) {
+        return BadRequestException({ message: 'invalid token22' })
     }
+
+
+    req.userId = decodedData.id
+    req.jti = decodedData.jti
+
+    let blocked_Token = await get(`RevokeToken::${req.userId}::${req.jti}`)
+    if (blocked_Token !== null) {
+        return BadRequestException({ message: 'invalid token (blocked)' })
+    }
+
+
 
     next()
 
