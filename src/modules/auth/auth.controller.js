2a94412295d4475bed528b214101d7bbceda8410
Ahmed Mohamed <ahmed.mohamed.connect@gmail.com>
2026-04-05 17:38:28 +0200

    problem cose defrant domain with google sign up

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -50,11 +50,15 @@ router.post('/signup/gmail', async (req, res) => {
 
     //? i used this email ==> ahmed.mohamed.connect@gmail.com
 
-    const csrfCookie = getCookieValue(req, "g_csrf_token");
-    const csrfBody = req.body?.g_csrf_token;
-    if (!csrfCookie || !csrfBody || csrfCookie !== csrfBody) {
-        return ConflictException({ message: "Invalid Google CSRF token" });
-    }
+
+    //this dont work till front and back in same domain or local host
+
+    // const csrfCookie = getCookieValue(req, "g_csrf_token");
+    // const csrfBody = req.body?.g_csrf_token;
+    // if (!csrfCookie || !csrfBody || csrfCookie !== csrfBody) {
+    //     return ConflictException({ message: "Invalid Google CSRF token" });
+    // }
+
 
 
     let host = `${req.protocol}://${req.host}`;
